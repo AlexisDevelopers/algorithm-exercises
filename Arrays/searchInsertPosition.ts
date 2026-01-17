ccd25f2dcb84010b426e51e36032866fc55ecb00
Alexis De Los Santos <43856936+AlexisDevelopers@users.noreply.github.com>
2026-01-17 12:04:36 -0400

    Create searchInsertPosition.ts

---
+++ Arrays/searchInsertPosition.ts
@@ -0,0 +1,45 @@
+/**
+ * LeetCode #35 - Search Insert Position
+ * Link: https://leetcode.com/problems/search-insert-position/description/
+ *
+ * Given a sorted array of distinct integers and a target value,
+ * return the index if the target is found.
+ * If not, return the index where it would be inserted in order.
+ *
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ * Example 1:
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ *
+ * Example 2:
+ * Input: nums = [1,3,5,6], target = 2
+ * Output: 1
+ *
+ * Example 3:
+ * Input: nums = [1,3,5,6], target = 7
+ * Output: 4
+ *
+ * Constraints:
+ * - 1 <= nums.length <= 10^4
+ * - -10^4 <= nums[i] <= 10^4
+ * - nums contains distinct values sorted in ascending order
+ * - -10^4 <= target <= 10^4
+ *
+ * @time O(log n)
+ * @space O(1)
+ */
+
+function searchInsert(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  return left;
+}
